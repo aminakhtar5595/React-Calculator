215335d55eb1c3c3cd710fc106298d63a1a2d3ee
Amin Akhtar <aminakhtar5595@gmail.com>
2019-11-30 12:47:35 +0500

    Updates

---
+++ precache-manifest.eb5f2d717eba8b2bcfcbd44fc30b6c4e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5d23467f4dd596388a6864e051789b2",
+    "url": "/React-Calculator/index.html"
+  },
+  {
+    "revision": "7f85f24eed5c5bed6454",
+    "url": "/React-Calculator/static/css/main.2915beb2.chunk.css"
+  },
+  {
+    "revision": "685068d579458849f083",
+    "url": "/React-Calculator/static/js/2.434b294a.chunk.js"
+  },
+  {
+    "revision": "7f85f24eed5c5bed6454",
+    "url": "/React-Calculator/static/js/main.3d2106c3.chunk.js"
+  },
+  {
+    "revision": "ba82322475a86c7434e3",
+    "url": "/React-Calculator/static/js/runtime-main.7ff1caf2.js"
+  }
+]);
